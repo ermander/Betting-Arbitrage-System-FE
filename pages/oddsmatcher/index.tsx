4419d207a427d35391c902e7cc4582bb970823ca
Emanuele Bertuol <61063909+ermander@users.noreply.github.com>
2022-08-02 22:26:41 +0200

    no message

---
+++ pages/oddsmatcher/index.tsx
@@ -0,0 +1,10 @@
+import MainLayout from "../../layout/MainLayout"
+import type { NextPage } from "next"
+
+import style from "../../styles/Oddsmatcher.module.scss"
+
+const Oddsmatcher: NextPage = () => {
+  return <MainLayout />
+}
+
+export default Oddsmatcher
